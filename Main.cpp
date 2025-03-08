677871d3ce46391957ebe838ad7c16dff9ee84a7
GuilhermeHorta22 <ghorta800@gmail.com>
2025-03-07 21:51:56 -0300

    testando commit

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio2.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+int main(void)
+{
+	gotoxy(10,20);
+	printf("\nPrograma feito com sucesso!!!");
+}
